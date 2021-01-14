8d205636b873cf51aa6949f29f9f460bbc89f492
sudbasnet <sudbasnet10@gmail.com>
2021-01-13 23:06:58 -0600

    adding conversation functionality

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DIALOGS from './dialogs';
 import Terminal from './Terminal/Terminal';
 
-const Home: React.FC = () => {
-    return <Terminal />
+const initialDialog = DIALOGS["Start"]
+
+class Home extends Component {
+    state: { questions: [String], dialog: String, showDialog: Boolean };
+    // maybe its better to let App handle this. there is not alot of point 
+    // doing this here
+
+    constructor(props: { dialog: String, questions: [String] }) {
+        super(props);
+        this.state = {
+            dialog: props.dialog,
+            questions: props.questions,
+            showDialog: true
+        }
+    }
+
+
+    updateDialogHandler(): void {
+
+    }
+
+
+    render() {
+        return <Terminal />
+    }
 }
 
 export default Home;
